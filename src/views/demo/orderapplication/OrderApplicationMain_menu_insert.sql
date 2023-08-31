10bf4b03f52b8aae9f276c7975d24aecb0ca33a7
Yihui Zhang <zhangyihui@buaa.edu.cn>
2023-08-31 15:48:49 +0800

    权限配置

--- src/views/demo/orderapplication/OrderApplicationMain_menu_insert.sql
+++ src/views/demo/orderapplication/OrderApplicationMain_menu_insert.sql
@@ -24,3 +24,12 @@ VALUES ('2023082903286200135', '2023082903286200130', '导出excel_电商采购�
 -- 导入excel
 INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
 VALUES ('2023082903286200136', '2023082903286200130', '导入excel_电商采购月度申请表', NULL, NULL, 0, NULL, NULL, 2, 'orderapplication:order_application_main:importExcel', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-08-29 15:28:13', NULL, NULL, 0, 0, '1', 0);
+-- 提交
+INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
+VALUES ('2023082903286200137', '2023082903286200130', '提交电商采购月度申请表', NULL, NULL, 0, NULL, NULL, 2, 'orderapplication:order_application_main:submit', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-08-29 15:28:13', NULL, NULL, 0, 0, '1', 0);
+-- 撤回
+INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
+VALUES ('2023082903286200138', '2023082903286200130', '撤回电商采购月度申请表', NULL, NULL, 0, NULL, NULL, 2, 'orderapplication:order_application_main:revoke', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-08-29 15:28:13', NULL, NULL, 0, 0, '1', 0);
+-- 批量撤回
+INSERT INTO sys_permission(id, parent_id, name, url, component, is_route, component_name, redirect, menu_type, perms, perms_type, sort_no, always_show, icon, is_leaf, keep_alive, hidden, hide_tab, description, create_by, create_time, update_by, update_time, del_flag, rule_flag, status, internal_or_external)
+VALUES ('2023082903286200139', '2023082903286200130', '批量撤回电商采购月度申请表', NULL, NULL, 0, NULL, NULL, 2, 'orderapplication:order_application_main:revokeBatch', '1', NULL, 0, NULL, 1, 0, 0, 0, NULL, 'admin', '2023-08-29 15:28:13', NULL, NULL, 0, 0, '1', 0);
